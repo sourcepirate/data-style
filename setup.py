0a4fb3910b8016f15265a6ca6b5d1d2ce420afdb
sourcepirate <plasmashadowx@gmail.com>
2017-09-06 17:31:45 +0530

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('LICENSE') as f:
 
 setup(
     name='dataland',
-    version='1.0.3',
+    version='1.0.5',
     description='A jutsu of data style',
     long_description=README,
     author='Sourcepirate',
